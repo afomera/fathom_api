ebd645ee7aa8a65a8ca52b9ceaf15e69f24938d3
Andrea Fomera <afomera@hey.com>
2021-08-16 14:26:01 -0600

    Initial commit to save gem name

---
+++ lib/fathom_api.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "fathom_api/version"
+
+module FathomApi
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ test/test_helper.rb
@@ -0,0 +1,6 @@
+# frozen_string_literal: true
+
+$LOAD_PATH.unshift File.expand_path("../lib", __dir__)
+require "fathom_api"
+
+require "minitest/autorun"
